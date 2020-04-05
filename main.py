60d43dc785d1bd966afbb9f4e8f906f1d57ddd45
SpermesePython <34358370+SpermesePython@users.noreply.github.com>
2020-04-04 22:16:26 -0500

    Reset to older version

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# Copyright 2015, Google, Inc.
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-# this file except in compliance with the License. You may obtain a copy of the
-# License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable
-# law or agreed to in writing, software distributed under the License is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-# or implied. See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from functions import *
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -39,11 +28,6 @@ def root():
     savings = 0
     goal = 0
 
-    bmi = 0
-    category = ""
-
-    message = ""
-
     if request.method == "POST":
 
         if request.form.get("feet") != "":
@@ -55,7 +39,27 @@ def root():
         if request.form.get("weight") != "":
             weight = float(request.form.get("weight"))
 
-        # bmi, category = functions.calc_BMI(feet, inches, weight)
+        metric_Weight =  weight * 0.45
+
+        metric_Height = ((feet * 12) + inches) * 0.025
+
+        if (metric_Height == 0):
+            metric_Height = 1
+
+        bmi = metric_Weight / (metric_Height * metric_Height)
+
+        if (bmi < 18.5):
+            category = "Underweight"
+
+        elif ((bmi >= 18.5) and (bmi < 25)):
+            category = "Normal"
+
+        elif ((bmi >= 25) and (bmi < 30)):
+            category = "Overweight"
+
+        else:
+            category = "Obese"
+
 
         if request.form.get("age") != "":
             age = float(request.form.get("age"))
@@ -69,7 +73,21 @@ def root():
         if request.form.get("goal") != "":
             goal = float(request.form.get("goal"))
 
-        # message = functions.calc_RA(age, salary, savings, goal)
+        currentSavings = 0
+        currentAge = age
+
+        while ((currentSavings <= goal) and (currentAge < 100)):
+
+            savingsThisYr = salary * (savings / 100)
+            employerMatch = savingsThisYr * 0.35
+
+            currentSavings = currentSavings + (savingsThisYr + employerMatch)
+            currentAge = currentAge + 1
+
+        if (currentAge >= 100):
+            message = "You will die before meeting your savings goal..."
+        else:
+            message = "You will meet your savings goal at " + str(currentAge)
 
         return render_template('results.html', bmi = bmi, category = category, message = message)
     
@@ -88,4 +106,6 @@ if __name__ == '__main__':
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+
 
