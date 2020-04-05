9682d8ff542d0bf29831f7669bc6f1b4dc77d58c
SpermesePython <34358370+SpermesePython@users.noreply.github.com>
2020-04-04 20:52:23 -0500

    Added functions file

--- main.py
+++ main.py
@@ -23,51 +23,9 @@
 # limitations under the License.
 
 from flask import Flask, render_template, request
+from functions import *
 
 app = Flask(__name__)
-
-def calc_BMI(feet, inches, weight):
-    metric_Weight =  weight * 0.45
-
-    metric_Height = ((feet * 12) + inches) * 0.025
-
-    if (metric_Height == 0):
-        metric_Height = 1
-
-    bmi = metric_Weight / (metric_Height * metric_Height)
-
-    if (bmi < 18.5):
-        category = "Underweight"
-
-    elif ((bmi >= 18.5) and (bmi < 25)):
-        category = "Normal"
-
-    elif ((bmi >= 25) and (bmi < 30)):
-        category = "Overweight"
-
-    else:
-        category = "Obese"
-
-    return bmi, category
-
-def calc_RA(age, salary, savings, goal):
-    currentSavings = 0
-    currentAge = age
-
-    while ((currentSavings <= goal) and (currentAge < 100)):
-
-        savingsThisYr = salary * (savings / 100)
-        employerMatch = savingsThisYr * 0.35
-
-        currentSavings = currentSavings + (savingsThisYr + employerMatch)
-        currentAge = currentAge + 1
-
-    if (currentAge >= 100):
-        message = "You will die before meeting your savings goal..."
-    else:
-        message = "You will meet your savings goal at " + str(currentAge)
-
-    return message
 
 @app.route('/', methods = ["GET", "POST"])
 def root():
